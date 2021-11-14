ca06f5f499f6b63410b68ed2e6c1013f6fb3297e
Luis Garbelotti <luisgsgarbelotti@gmail.com>
2021-11-14 17:53:19 -0300

    Adicionando Requisitos de Entrada na Sala

--- script.js
+++ script.js
@@ -1,56 +1,141 @@
-let userName = prompt("Qual seu lindo nome?")
+let usuario = {};
 
 
-let promessa = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-promessa.then(carregarMensagens);
+function entrarNaSala() {
 
-let ultimoElemento = [];
+    let nomeUsuario = prompt("Qual seu lindo nome?");
 
-function carregarMensagens(resposta) {
-    console.log(resposta.data);
-    
-    imprimirMensagens(resposta.data);
+    usuario = {
+                name: nomeUsuario 
+            }
+
+    let promessaUsuario = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", usuario);
+    promessaUsuario.then(enviarRequsicaoDeMensagens);
+    promessaUsuario.catch(trataErroLogin);
+
+} entrarNaSala();
+
+
+const usuarioPresente = setInterval(manterConexao, 5000);
+const intervaloAtualizarMensagens = setInterval(enviarRequsicaoDeMensagens, 3000);
+
+
+function trataErroLogin () {
+
+    alert("Este nome já está em uso. Tente Novamente.");
+    entrarNaSala();
+
 }
 
+
+function manterConexao () {
+
+    const promessaEnviarStatus = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", usuario);
+    promessaEnviarStatus.catch(desconectado);
+
+}
+
+
+function desconectado () {
+
+    alert("Você foi desconectado. Entre novamente.");
+    entrarNaSala();
+
+}
+
+
+function enviarRequsicaoDeMensagens (response) {
+
+    let promessaMensagens = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    promessaMensagens.then(carregarMensagens);
+
+} 
+
+
+function carregarMensagens(resposta) {
+
+    imprimirMensagens(resposta.data);
+
+}
+
+
 function imprimirMensagens(todasMensagens) {
+
+    const ulMensagem = document.querySelector(".historicoDeMensagens");
+    ulMensagem.innerHTML = "";
+
     for (let i = 0; i < todasMensagens.length; i++){
 
         if( todasMensagens[i].type === 'status') {
-            const ulMensagemStatus = document.querySelector(".historicoDeMensagens");
-            ulMensagemStatus.innerHTML += `<li class="mensagem mensagemStatus">
-                                            <span class="horario">  (${todasMensagens[i].time})   </span> 
-                                            <span> <strong>  ${todasMensagens[i].from}  </strong> </span> 
-                                            <span> ${todasMensagens[i].text} </span> 
+            if (i === todasMensagens.length - 1) {
+                ulMensagem.innerHTML += `<li class="mensagem mensagemStatus ultimaMensagem">
+                                                <span class="horario">  (${todasMensagens[i].time})   </span> 
+                                                <span> <strong>  ${todasMensagens[i].from}  </strong> </span> 
+                                                <span> ${todasMensagens[i].text} </span> 
                                             </li>
-                                            `
-        }
+                                            `;
+
+            } else {
+                ulMensagem.innerHTML += `<li class="mensagem mensagemStatus">
+                                                <span class="horario">  (${todasMensagens[i].time})   </span> 
+                                                <span> <strong>  ${todasMensagens[i].from}  </strong> </span> 
+                                                <span> ${todasMensagens[i].text} </span> 
+                                            </li>
+                                            `;
+            }                       
+        } 
         
         if (todasMensagens[i].type === 'message') {
-            const ulMensagemNormal = document.querySelector(".historicoDeMensagens");
-            ulMensagemNormal.innerHTML += `<li class="mensagem mensagemNormal">
-                                            <span class="horario"> (${todasMensagens[i].time}) </span> 
-                                            <span> <strong> ${todasMensagens[i].from} </strong> </span> 
-                                            <span> para </span>
-                                            <span> <strong>${todasMensagens[i].to} </strong> </span>
-                                            <span> ${todasMensagens[i].text} </span>
-                                            </li>
-                                            `
-        }
-        
-        if(todasMensagens[i].type === 'private_message') {
-            const ulMensagemPrivada = document.querySelector(".historicoDeMensagens");
-            ulMensagemPrivada.innerHTML += `<li class="mensagem mensagemPrivada">
+            if (i === todasMensagens.length - 1) {
+                ulMensagem.innerHTML += `<li class="mensagem mensagemNormal ultimaMensagem">
                                                 <span class="horario"> (${todasMensagens[i].time}) </span> 
                                                 <span> <strong> ${todasMensagens[i].from} </strong> </span> 
                                                 <span> para </span>
                                                 <span> <strong>${todasMensagens[i].to} </strong> </span>
                                                 <span> ${todasMensagens[i].text} </span>
-                                                </li>
-                                                `        
-            }  
+                                            </li>
+                                            `;
+            } else {
+                ulMensagem.innerHTML += `<li class="mensagem mensagemNormal ">
+                                                <span class="horario"> (${todasMensagens[i].time}) </span> 
+                                                <span> <strong> ${todasMensagens[i].from} </strong> </span> 
+                                                <span> para </span>
+                                                <span> <strong>${todasMensagens[i].to} </strong> </span>
+                                                <span> ${todasMensagens[i].text} </span>
+                                            </li>
+                                            `;
+            }
+        } 
+        
+        if(todasMensagens[i].type === 'private_message') {
+            if (i === todasMensagens.length - 1) {
+                ulMensagem.innerHTML += `<li class="mensagem mensagemPrivada ultimaMensagem">
+                                                <span class="horario"> (${todasMensagens[i].time}) </span> 
+                                                <span> <strong> ${todasMensagens[i].from} </strong> </span> 
+                                                <span> para </span>
+                                                <span> <strong>${todasMensagens[i].to} </strong> </span>
+                                                <span> ${todasMensagens[i].text} </span>
+                                            </li>
+                                            `;
+            } else {
+                ulMensagem.innerHTML += `<li class="mensagem mensagemPrivada">
+                                                <span class="horario"> (${todasMensagens[i].time}) </span> 
+                                                <span> <strong> ${todasMensagens[i].from} </strong> </span> 
+                                                <span> para </span>
+                                                <span> <strong>${todasMensagens[i].to} </strong> </span>
+                                                <span> ${todasMensagens[i].text} </span>
+                                            </li>
+                                            `;
+            }
+        }
     }
-    
+
+
+const irParaUltimaMensagem = document.querySelector(".ultimaMensagem");
+irParaUltimaMensagem.scrollIntoView();
+
 }
+
 
 
                                     
